122e97d370c44c07873ea1bf1277d4b57a7ccbcd
points14514 <3790010758@qq.com>
2026-02-19 09:59:42 +0800

    Add downloads for GCC, Binutils, and Glibc

--- autogen.sh
+++ autogen.sh
@@ -1,4 +1,7 @@
 wget https://ftp.gnu.org/gnu/make/make-4.4.1.tar.gz
+wget https://ftp.gnu.org/gnu/gcc/gcc-11.4.0/gcc-11.4.0.tar.xz
+wget https://ftp.gnu.org/gnu/binutils/binutils-2.46.0.tar.xz
+wget https://ftp.gnu.org/gnu/glibc/glibc-2.35.0.tar.bz2
 tar *.tar.*
 rm -rf *.tar.*
 echo "ok"
