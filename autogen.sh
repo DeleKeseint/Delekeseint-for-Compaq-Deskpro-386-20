525df8fe6d0f961b6931cdc763e93faf4226eadf
points14514 <3790010758@qq.com>
2026-02-19 10:09:01 +0800

    Add download link for Linux kernel 5.4.302

--- autogen.sh
+++ autogen.sh
@@ -2,6 +2,7 @@ wget https://ftp.gnu.org/gnu/make/make-4.4.1.tar.gz
 wget https://ftp.gnu.org/gnu/gcc/gcc-11.4.0/gcc-11.4.0.tar.xz
 wget https://ftp.gnu.org/gnu/binutils/binutils-2.46.0.tar.xz
 wget https://ftp.gnu.org/gnu/glibc/glibc-2.35.0.tar.bz2
+wget https://www.kernel.org/pub/linux/kernel/v5.x/linux-5.4.302.tar.xz
 tar *.tar.*
 rm -rf *.tar.*
 echo "ok"
